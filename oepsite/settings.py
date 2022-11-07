0472a0d49727953e44c2162b9788ab64cdc4dff2
Роман Кислов <romankislov@MacBook-Air-Roman.local>
2022-11-07 14:47:06 +0500

    pre release-9

--- oepsite/settings.py
+++ oepsite/settings.py
@@ -130,10 +130,10 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
